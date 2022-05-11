cbe8d9ff99f3ccc3a16431e6e3e81da307a4db11
Marcos Paulo Rodrigues <marcos.paulo.rr@outlook.com>
2022-05-10 22:04:42 -0300

    projeto

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/orcamento', [App\Http\Controllers\PlaniumController::class, 'getOrcamento'])->name('orcamento');
