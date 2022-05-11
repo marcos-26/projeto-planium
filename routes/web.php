3efff660f7c29350d5b65eb25109312f3b77b4d5
Marcos Paulo Rodrigues <marcos.paulo.rr@outlook.com>
2022-05-10 22:46:31 -0300

    subir projeto

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/orcamento', [App\Http\Controllers\PlaniumController::class, 'getOrcamento'])->name('orcamento');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
